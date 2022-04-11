b703cfcb1abb5ab072cbb8b8654f87ec1a05a1aa
CB-BLACK <102660320+CB-BLACK@users.noreply.github.com>
2022-04-11 12:06:57 +0200

    Add files via upload

---
+++ tarea-sesion04/ejercicio2.js
@@ -0,0 +1,5 @@
+// Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.
+
+let cadena = "Este es el ejercicio 2 de la sesion 4 de Javascript de OpenBootcamp";
+
+console.log(cadena.toUpperCase());
