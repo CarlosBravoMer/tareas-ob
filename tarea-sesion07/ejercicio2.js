c1b4e39ef4d47fb24f117276e704d6c21ad0e0f0
CB-BLACK <102660320+CB-BLACK@users.noreply.github.com>
2022-04-17 15:16:52 +0200

    Update ejercicio2.js

--- tarea-sesion07/ejercicio2.js
+++ tarea-sesion07/ejercicio2.js
@@ -2,9 +2,9 @@
 
 class Coche {
     constructor(peso, potencia, marca){
-    this.peso = peso;
-    this.potencia = potencia;
-    this.marca = marca;
+    this._peso = peso;
+    this._potencia = potencia;
+    this._marca = marca;
 
     console.log(`Peso ${peso}kg, ${potencia} caballos y Marca ${marca}`);
 
