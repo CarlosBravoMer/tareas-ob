70a51e99a5cec15701df653214f95462ed35dba3
CB-BLACK <102660320+CB-BLACK@users.noreply.github.com>
2022-04-26 22:07:55 +0200

    Create app.js

---
+++ tarea-sesion15/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+
+app.use(express.static(__dirname + '/controllers/'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
